e11ff372d3eb4da52a642efe510ab764c71162e8
Timo Wendner <timo.wendner@gmx.net>
2023-03-27 16:50:34 +0200

    log

--- audio2mel.py
+++ audio2mel.py
@@ -34,7 +34,7 @@ class AudioToMelSpectrogram(torch.nn.Module):
 
         # # Convert to decibels
         # spec = F.amplitude_to_DB(spec, 20, 1e-10, np.log10(max(spec.max(), 1e-10)))
-        # spec = torch.log2(spec)
+        spec = torch.log2(spec)
         
         # ATDB = T.AmplitudeToDB(stype="amplitude", top_db=80)
         # spec = ATDB(spec)
@@ -51,7 +51,7 @@ class AudioToMelSpectrogram(torch.nn.Module):
         Returns:
             torch.tensor: The original Waveform
         """
-        # spectrogram = torch.pow(spectrogram, 2)
+        spectrogram = torch.pow(spectrogram, 2)
         spectrogram = self.inverse_mel_scale(spectrogram)
         waveform = self.griffin_lim(spectrogram)
         return waveform
