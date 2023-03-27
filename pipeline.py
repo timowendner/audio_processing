edbdbfce93142bb17f82a09586605f9dc98c2355
Timo Wendner <timo.wendner@gmx.net>
2023-03-27 16:21:21 +0200

    test

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@ import torchaudio
 from audio_preprocessing import AudioPreprocessor
 from audio2mel import AudioToMelSpectrogram
 import matplotlib.pyplot as plt
+%matplotlib inline
 
 def main():
     # Load the input wavefile
